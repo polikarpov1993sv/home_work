9d311063001eeadf5bc513463a6e8317bc29e36d
Sergey Polikarpov <polikarpov1993sv@gmail.com>
2022-10-15 00:02:41 +0400

    for verification 14.10.202

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 grades = {"Математика" : [2, 5, 7, 8], "Русский" : [2, 5, 6, 7]}
 gradesr = {"Математика" : [1, 5, 7, 8], "Русский" : [2, 5, 6, 7]}
 
-def av_rating(grades):
-    sum_rating = 0
-    len_rating = 0
-    for course in grades.values():
-        sum_rating += sum(course)
-        len_rating += len(course)
-    average_rating = sum_rating / len_rating
-    print(average_rating)
 
-av_rating(grades)
+sum_rating = 0
+len_rating = 0
+for lesson in grades.keys():
+    key = "Русский"
+    if lesson == key:
+        print(sum(grades[key]))
+
