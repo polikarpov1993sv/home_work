17626b8dc801c024fa1d4ca3aacfb0650eec2e69
Sergey Polikarpov <polikarpov1993sv@gmail.com>
2022-10-25 01:13:41 +0400

    24/10/2022

---
+++ HomeWork_3.9/Задание_1.py
@@ -0,0 +1,32 @@
+recipes = []
+
+with open('recipes.txt', 'rt', encoding='utf8' ) as rec_fail:
+    for text in rec_fail:
+        dish_name = text.strip()
+        dish = {"name": dish_name, "ingredients": []}
+        ingredients_count = rec_fail.readline()
+        for ing in range(int(ingredients_count)):
+            ingredients = rec_fail.readline()
+            name, quantity, measure = ingredients.strip().split(' | ')
+            dish ['ingredients'].append({'name': name, 'quantity': quantity, 'measure': measure})
+        blank_line = rec_fail.readline()
+        recipes.append(dish)
+
+def get_shop_list_by_dishes(list_dish, people):
+    purchases = {}
+    for dish in list_dish:
+        for dish_book in recipes:
+            if dish_book['name'] == dish:
+                for ing in dish_book['ingredients']:
+                    if ing['name'] not in purchases.keys():
+                        measure = ing['measure']
+                        quantity = people * int(ing['quantity'])
+                        purchases[ing['name']] = {'measure': measure, 'quantity': quantity}
+                    
+                    elif ing['name'] in purchases.keys():
+                        measure = ing['measure']
+                        quantity = people * int(ing['quantity']) + int((purchases[ing['name']])['quantity'])
+                        purchases[ing['name']] = {'measure': measure, 'quantity': quantity}
+    return purchases
+
+print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2))
