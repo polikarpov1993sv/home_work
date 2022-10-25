152eecd968b658f4091016757c003dba36369d89
Sergey Polikarpov <polikarpov1993sv@gmail.com>
2022-10-25 19:58:25 +0400

    25/10/2022_2

--- HomeWork_3.9/Задание_3/Задание_3.py
+++ HomeWork_3.9/Задание_3/Задание_3.py
@@ -1,17 +1,28 @@
 text_list = ['1.txt', '2.txt', '3.txt']
 
 text_read = {}
+text_lines = {}
+out_text_List = []
+for text_file in text_list:
+    with open(text_file, 'rt', encoding='utf8' ) as t:
+        text = t.read()
+        lines = text.count('\n') + 1
+        text_lines[text_file] = lines
 
-for text in text_list:
-    with open(text, 'rt', encoding='utf8' ) as text_print:
-        text_read[text] = text_print.read()
+for text_file in text_list:
+    with open(text_file, 'rt', encoding='utf8' ) as t:
+        text_read[text_file] = t.read()
 
-for text_el in text_read.values():
-    quantity = 0
-    if text_el == "\n":
-    # Найти команду для \n
-        quantity += 1
-    print(quantity)
+sorted_text_line = {}
+sorted_key_text_line = sorted(text_lines, key=text_lines.get)
 
-# разобраться с упорядовачинием словаря
-# разобраться с выводом
+for i in sorted_key_text_line:
+    sorted_text_line[i] = text_lines[i]
+
+for num_text in sorted_text_line.keys():
+    out_text_List.append(f'{num_text}\n{sorted_text_line[num_text]}\n{text_read[num_text]}')
+
+out_text = "\n".join(out_text_List)
+
+with open("out_text.txt", 'w', encoding='utf8' ) as doc:
+    doc.write(out_text)
