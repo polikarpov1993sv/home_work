04c288e91465fb283494809a7ee53ca1882d451f
Sergey Polikarpov <polikarpov1993sv@gmail.com>
2023-01-20 05:04:22 +0400

    20.01.2023 05:00

--- HomeWork_6.4/todo1.py
+++ HomeWork_6.4/todo1.py
@@ -5,18 +5,16 @@ class FlatIterator:
 
     def __iter__(self):
         self.list = []
+        if len(self.list_of_list) > 0:
+            for el in self.list_of_list:
+                self.list.extend(el)
         return self
 
     def __next__(self):
-        if len(self.list_of_list) == 0 and len(self.list) == 0:
+        if len(self.list) == 0:
             raise StopIteration
-        if len(self.list) == 0 and len(self.list_of_list) > 0:
-            self.list.extend(self.list_of_list.pop(0))
         item =  self.list.pop(0)
         return item
-
-list_of_lists_1 = [['a', 'b', 'c'],['d', 'e', 'f', 'h', False],[1, 2, None]]
-
 
 
 def test_1():
@@ -38,7 +36,5 @@ def test_1():
 
 
 
-# print(test_1())
-
-# if __name__ == '__main__':
-#     test_1()
+if __name__ == '__main__':
+    test_1()
